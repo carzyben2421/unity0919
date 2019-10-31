b2403b8da1e80d41bc8aa7e2daaabde6d60f6a7a
carzyben2421 <55227126+carzyben2421@users.noreply.github.com>
2019-10-31 14:51:02 +0800

    1031

--- Unity0919/Assets/Game/GameManager.cs
+++ Unity0919/Assets/Game/GameManager.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
    public AudioMixer mixer;
-    public void SetVbgm(float value)
+
+    public Text loadingText;
+    public Slider loading;
+
+    public void SetVBGM(float value)
     {
         mixer.SetFloat("VBGM", value);
+    }
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("AK47");
     }
 }

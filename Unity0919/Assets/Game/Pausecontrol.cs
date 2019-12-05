bc9cc52ddbc18f78ae59dc12e1bbdbe70b170b77
carzyben2421 <55227126+carzyben2421@users.noreply.github.com>
2019-12-05 14:49:58 +0800

    1205

---
+++ Unity0919/Assets/Game/Pausecontrol.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pausecontrol : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image imagePause;
+    public Sprite spritPause, spritePlay;
+    [Header("暫停")]
+    public bool pause;
+
+    private void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.CapsLock))
+        { 
+            print("暫停");
+            pause = !pause;
+
+            imagePause.sprite = pause ? spritePlay : spritPause;
+            Time.timeScale = pause ? 0 : 1;
+        }
+    }
+
+    private void Update()
+    {
+        Pause();
+    }
+}
+
+

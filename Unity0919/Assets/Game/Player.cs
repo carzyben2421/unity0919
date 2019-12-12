a88cbc8290bcfac648a19b3cf3e5789d9becf4e2
carzyben2421 <55227126+carzyben2421@users.noreply.github.com>
2019-12-12 14:52:38 +0800

    1212

---
+++ Unity0919/Assets/Game/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int hp = 100;
+    public Slider hpslider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpslider.value = hp;
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpslider.value = hp;
+        }
+    }
+
+}

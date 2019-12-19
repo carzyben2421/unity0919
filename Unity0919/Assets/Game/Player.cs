f8124689aae278efb0e34fd0e8ba4a70616bd40e
carzyben2421 <55227126+carzyben2421@users.noreply.github.com>
2019-12-19 15:38:18 +0800

    1219

--- Unity0919/Assets/Game/Player.cs
+++ Unity0919/Assets/Game/Player.cs
@@ -6,6 +6,13 @@ public class Player : MonoBehaviour
     public int hp = 100;
     public Slider hpslider;
 
+    public Text textTacos;
+    public int TacosCount;
+    public int TacosTotal;
+
+    public Text textTime;
+    public float gameTime;
+
     private void OnTriggerEnter(Collider other)
     {
        if (other.tag == "陷阱")
@@ -13,6 +20,18 @@ public class Player : MonoBehaviour
             int d = other.GetComponent<Trap>().damage;
             hp -= d;
             hpslider.value = hp;
+        }
+
+        if (other.tag == "Tacos")
+        {
+            TacosCount++;
+            textTacos.text = "Tacos : " + TacosCount + " / " + TacosTotal;
+            Destroy(other.gameObject);
+        }
+
+        if (other.name =="終點" && TacosCount == TacosTotal)
+        {
+            print("過關");
         }
     }
     private void OnParticleCollision(GameObject other)
@@ -25,4 +44,20 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        TacosTotal = GameObject.FindGameObjectsWithTag("Tacos").Length;
+        textTacos.text = "Tacos : 0 / " + TacosTotal;
+    }
+
+    private void Update()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("F2");
+    }
 }
